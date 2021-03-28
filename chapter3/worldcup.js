4b2dff1267095deea2442a1022eb56e3b5a06455
Alex Incerti <alex.incerti@outlook.com>
2021-03-28 19:10:02 +0200

    middle chapter 3

---
+++ chapter3/worldcup.js
@@ -0,0 +1,89 @@
+d3.csv("/data/worldcup.csv", data => createViz(data))
+
+function createViz(data){
+    console.log(data)
+
+    visualizationG = d3.select("#viz")
+        .select("svg")
+        .append("g")
+        .attr("id", "teamsG")
+        .attr("transform", "translate(" + 75 + ", " + 240 + ")")
+
+    visualizationG
+        .selectAll("g")
+        .data(data, d=>d.team)
+        .enter()
+        .append('g')
+        .attr("class", "overallG")
+        .attr('transform', (d,i) => "translate(" + i * 50 + ", " + 0 + ")")
+
+    initializeCircles(data, "win")
+
+    buttonsG = d3.select("#controls")
+        .selectAll("button")
+        .data(Object.keys(data[0]).filter(d => !["team", "region"].includes(d)))
+        .enter()
+        .append("button")
+        .text(d => d)
+        .on("click", d => populateCircles(data, d))
+
+    function populateCircles(data, attribute){
+        radiusScale = 
+            d3.scaleLinear()
+            .domain([0, d3.max(data.map(d=>parseInt(d[attribute])))])
+            .range([0, 30])
+
+        d3.selectAll("g.overallG")
+            .select('circle')
+            .transition()
+            .delay((d,i) => i * 100)
+            .duration(1000)
+            .attr('r', d => radiusScale(parseInt(d[attribute])))
+        }
+
+    function initializeCircles(data, attribute){
+        radiusScale = 
+            d3.scaleLinear()
+            .domain([0, d3.max(data.map(d=>parseInt(d[attribute])))])
+            .range([0, 30])
+
+        visualizationG
+            .selectAll("g")
+            .append('circle')
+            .attr("class", "inactive")
+            .attr("r", 0)
+            .transition()
+            .delay((d,i) => i * 50)
+            .attr('r', 50)
+            .transition()
+            .delay((d,i) => i * 10)
+            .attr('r', d => radiusScale(parseInt(d[attribute])))
+        
+        visualizationG
+            .selectAll("g")
+            .selectAll('circle')
+            .on("mouseover", function(d,i){
+            d3.select(this)
+                .transition()
+                .duration(400)
+                .attr("r", d => radiusScale(d[attribute]*2))
+            d3.select(this.parentElement).raise();
+            region = d.region
+            d3.selectAll("g.overallG")
+                .select('circle')
+                .classed("active", d=>d.region == region)
+                .classed("inactive", d=>d.region != region)
+            })
+            .on("mouseout", function(d){
+                d3.selectAll("g.overallG")
+                    .select('circle')
+                    .classed("active", false)
+                    .classed("inactive", true)
+            })
+        
+        d3.selectAll("g.overallG")
+            .append("text")
+            .text(d => d.team)
+            .attr('transform', (d,i) => "translate(" + 0 +", " + 70 + ")")
+    }
+}
--- notes.md
+++ notes.md
@@ -7,3 +7,12 @@
 * **clamp(true)**: allows to impose d3 not to extrapolate values greater than the maximum domain in a scale. Greater values are mapped to the highest value of the domain
 
 * **data(<data>, function_id)**: allows to specify what id should be used to bind shapes to data object. By default d3 uses the position in the array, which is in most of the cases not what we desire. Better always to define a meaningful id.
+
+### Chapter 3
+
+* To access the HTML element associated with a datapoint one can use **this** but **not in arrow functions** (only in `function(d){}` and not `(d)=>{}`). Alternatively **node()** of a selection (`d3.select("circle").node();`)
+
+* SVG has no Z-levels, which means that the drawing order of elements is determined by their DOM order.
+In order to avoid having the element below the others (for example in case this elements is enlarged and overlaps the neighobors) d3 functions can be used `<d3selectiom>.raise()` or lower(). Make sure you are selecting the right level. In case not navigate the tree using `d3.select(this.parentElement)` and raise that. Alternatively, in native JS `this.parentElement.appendChild(this);`
+
+* `style("pointer-events","none");` can be used to disable events (mouseover, out) fired by this element
